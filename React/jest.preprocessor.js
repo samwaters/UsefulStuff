cfd85e23f6f82b90a45c36024bd3256bed56afe3
Sam Waters <sam@samwaters.com>
2017-06-27 11:52:22 +0100

    Update jest preprocessor to use different css module

--- React/jest.preprocessor.js
+++ React/jest.preprocessor.js
@@ -3,40 +3,40 @@
  * Converts the CSS to a hashmap containing the selectors
  * Used to simulate css-loader in Webpack
  */
-const parser = require('cssparser');
+const cssjson = require('cssjson');
 const sass = require('node-sass');
 
 module.exports = {
-	getSelectors(rules) {
-		let selectors = {};
-		rules.forEach(rule => {
-			if(rule.type === 'style') {
-				// Remove the first . or # (if there is one)
-				let selectorName = rule.selector.replace(/^[.#]/, '');
-				// Remove any :pseudo
-				selectorName = selectorName.replace(/:[A-Za-z0-9()-]+/g, '');
-				// No spaces for you
-				selectorName = selectorName.replace(/\s+/g, '');
-				// Remove any [ ] attrs
-				selectorName = selectorName.replace(/\[[^\]]+\]/, '');
-				// Split any selectors up
-				let selectorTree = selectorName.split(/[^A-Za-z0-9_-]/);
-				// Add them all into the selector list
-				selectorTree.forEach(sT => selectors[sT] = sT);
-			}
-			if(rule.children) {
-				Object.assign(selectors, this.getSelectors(rule.children));
-			}
-		});
-		return selectors;
-	},
-	process(src, path) {
-		let stylesObject = {};
-		if(/\.s?css$/i.test(path)) {
-			let result = sass.renderSync({file: path});
-			let parsed = parser.parse(result.css.toString('utf8'));
-			stylesObject = this.getSelectors(parsed.rulelist);
-		}
-		return 'module.exports = ' + JSON.stringify(stylesObject);
-	}
+    getSelectors(rules) {
+        let selectors = {};
+        Object.keys(rules).forEach(rule => {
+            if(rule.charAt(0) !== '@') {
+                // Remove the first . or # (if there is one)
+                let selectorName = rule.replace(/^[.#]/, '');
+                // Remove any :pseudo
+                selectorName = selectorName.replace(/:[A-Za-z0-9()-]+/g, '');
+                // No spaces for you
+                selectorName = selectorName.replace(/\s+/g, '');
+                // Remove any [ ] attrs
+                selectorName = selectorName.replace(/\[[^\]]+\]/, '');
+                // Split any selectors up
+                let selectorTree = selectorName.split(/[^A-Za-z0-9_-]/);
+                // Add them all into the selector list
+                selectorTree.forEach(sT => selectors[sT] = sT);
+            }
+            if(rules[rule].children) {
+                Object.assign(selectors, this.getSelectors(rules[rule].children));
+            }
+        });
+        return selectors;
+    },
+    process(src, path) {
+        let stylesObject = {};
+        if(/\.s?css$/i.test(path)) {
+            let result = sass.renderSync({file: path});
+            let parsed = cssjson.toJSON(result.css.toString('utf8'));
+            stylesObject = this.getSelectors(parsed.children);
+        }
+        return 'module.exports = ' + JSON.stringify(stylesObject);
+    }
 };
